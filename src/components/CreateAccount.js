c02183f48d15afd0777f57c74341197246ecb322
TOPAiiN <topaiin@TOPAiiNs-MacBook-Pro.local>
2021-08-18 18:19:29 -0400

    uncomment line 28 from CreateAccount component

--- src/components/CreateAccount.js
+++ src/components/CreateAccount.js
@@ -25,7 +25,7 @@ function CreateAccount(){
       if (!validate(name,     'name'))     return;
       if (!validate(email,    'email'))    return;
       if (!validate(password, 'password')) return;
-    //   ctx.users.push({name,email,password,balance:100});
+      ctx.users.push({name,email,password,balance:100});
       setShow(false);
     }    
   
