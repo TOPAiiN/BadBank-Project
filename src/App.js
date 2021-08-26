42cf2e588656fed46f83a1bea351268faf0bce13
TOPAiiN <topaiin@TOPAiiNs-MacBook-Pro.local>
2021-08-26 15:03:54 -0400

    login and balance components deleted

--- src/App.js
+++ src/App.js
@@ -1,32 +1,27 @@
 import React, { useContext } from 'react';
 import { HashRouter, BrowserRouter, Route, Link } from "react-router-dom";
-import './index.css';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar2 from './NavBar'
 import CreateAccount from './components/CreateAccount';
-import Login from './components/Login';
 import Deposit from './components/Deposit';
 import Withdraw from './components/Withdraw';
-import Balance from './components/Balance';
 import AllData from './components/AllData';
 import { UserContext } from "./context";
 import Home from './components/Home';
 import ReactDOM from 'react-dom';
-
+import './index.css';
 
 function App() {
   return (
     <HashRouter>
       <NavBar2/>
       <UserContext.Provider value={{users:[{name:'probando',email:'aloha@prueba.cl',password:'secret',balance:100}]}}>
-        <div className="container" style={{padding: "20px"}}>
+        <div className="container navbar-menu" style={{padding: "20px"}}>
           <Route path="/" exact component={Home} />
           <Route path="/CreateAccount/" component={CreateAccount} />
-          <Route path="/login/" component={Login} />
           <Route path="/deposit/" component={Deposit} />
           <Route path="/withdraw/" component={Withdraw} />
-          <Route path="/balance/" component={Balance} />
           <Route path="/alldata/" component={AllData} />
         </div>
       </UserContext.Provider>      
